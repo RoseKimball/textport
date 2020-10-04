5c8c2929b238c2f708e86b5e3b2ef5de903cd897
Inika Chikarmane <64404812+inika-c@users.noreply.github.com>
2020-10-05 00:10:45 +0530

    Update stylesheet.js

--- textport/js/stylesheet.js
+++ textport/js/stylesheet.js
@@ -1,20 +1,22 @@
 /*global _storage*/
 
 /*
- * This file is part of Super Simple Highlighter.
+ * This file is a part of textport.
  * 
- * Super Simple Highlighter is free software: you can redistribute it and/or modify
+ * textport is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  * 
- * Super Simple Highlighter is distributed in the hope that it will be useful,
+ * textport is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  * 
  * You should have received a copy of the GNU General Public License
  * along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * textport was built with the help of einzelcode's Super Simple Highlighter repository (covered under the same license)
  */
 
 
